f5b3364e1ee6139c47e0fd32db07ae6361623595
Ian Macartney <ianhmacartney@gmail.com>
2024-12-01 23:42:54 -0800

    initial

---
+++ src/client/index.ts
@@ -0,0 +1,108 @@
+import {
+  Expand,
+  FunctionReference,
+  GenericDataModel,
+  GenericMutationCtx,
+  GenericQueryCtx,
+  mutationGeneric,
+  queryGeneric,
+} from "convex/server";
+import { GenericId, v } from "convex/values";
+import { api } from "../component/_generated/api";
+
+export class Prosemirror<Shards extends Record<string, number>> {
+  constructor(
+    public component: UseApi<typeof api>,
+    public options?: { shards?: Shards; defaultShards?: number }
+  ) {}
+  async add<Name extends string = keyof Shards & string>(
+    ctx: RunMutationCtx,
+    name: Name,
+    count: number = 1
+  ) {
+    const shards = this.options?.shards?.[name] ?? this.options?.defaultShards;
+    return ctx.runMutation(this.component.lib.add, {
+      name,
+      count,
+      shards,
+    });
+  }
+  async count<Name extends string = keyof Shards & string>(
+    ctx: RunQueryCtx,
+    name: Name
+  ) {
+    return ctx.runQuery(this.component.lib.count, { name });
+  }
+  // Another way of exporting functionality
+  for<Name extends string = keyof Shards & string>(name: Name) {
+    return {
+      add: async (ctx: RunMutationCtx, count: number = 1) =>
+        this.add(ctx, name, count),
+      subtract: async (ctx: RunMutationCtx, count: number = 1) =>
+        this.add(ctx, name, -count),
+      inc: async (ctx: RunMutationCtx) => this.add(ctx, name, 1),
+      dec: async (ctx: RunMutationCtx) => this.add(ctx, name, -1),
+      count: async (ctx: RunQueryCtx) => this.count(ctx, name),
+    };
+  }
+  /**
+   * For easy re-exporting.
+   * Apps can do
+   * ```ts
+   * export const { add, count } = counter.api();
+   * ```
+   */
+  api() {
+    return {
+      add: mutationGeneric({
+        args: { name: v.string() },
+        handler: async (ctx, args) => {
+          await this.add(ctx, args.name);
+        },
+      }),
+      count: queryGeneric({
+        args: { name: v.string() },
+        handler: async (ctx, args) => {
+          return await this.count(ctx, args.name);
+        },
+      }),
+    };
+  }
+}
+
+/* Type utils follow */
+
+type RunQueryCtx = {
+  runQuery: GenericQueryCtx<GenericDataModel>["runQuery"];
+};
+type RunMutationCtx = {
+  runMutation: GenericMutationCtx<GenericDataModel>["runMutation"];
+};
+
+export type OpaqueIds<T> = T extends GenericId<infer _T> | string
+  ? string
+  : T extends (infer U)[]
+    ? OpaqueIds<U>[]
+    : T extends ArrayBuffer
+      ? ArrayBuffer
+      : T extends object
+        ? { [K in keyof T]: OpaqueIds<T[K]> }
+        : T;
+
+export type UseApi<API> = Expand<{
+  [mod in keyof API]: API[mod] extends FunctionReference<
+    infer FType,
+    "public",
+    infer FArgs,
+    infer FReturnType,
+    infer FComponentPath
+  >
+    ? FunctionReference<
+        FType,
+        "internal",
+        OpaqueIds<FArgs>,
+        OpaqueIds<FReturnType>,
+        FComponentPath
+      >
+    : UseApi<API[mod]>;
+}>;
