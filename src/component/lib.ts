d2fb7ba7b1cebc0aaa439033fd75837763c46b88
Ian Macartney <ianhmacartney@gmail.com>
2024-12-02 01:45:49 -0800

    first pass at lib functions

--- src/component/lib.ts
+++ src/component/lib.ts
@@ -1,44 +1,174 @@
 import { v } from "convex/values";
-import { mutation, query } from "./_generated/server";
+import { mutation, query, QueryCtx } from "./_generated/server";
+import { Doc } from "./_generated/dataModel";
 
-export const add = mutation({
+const MAX_DELTA_FETCH = 1000;
+
+export const sync = mutation({
   args: {
-    name: v.string(),
-    count: v.number(),
-    shards: v.optional(v.number()),
+    id: v.string(),
+    version: v.number(),
+    clientId: v.string(),
+    steps: v.array(v.string()),
   },
-  returns: v.null(),
+  returns: v.union(
+    v.object({
+      status: v.literal("needs-rebase"),
+      clientIds: v.array(v.string()),
+      steps: v.array(v.string()),
+    }),
+    v.object({ status: v.literal("synced") })
+  ),
   handler: async (ctx, args) => {
-    const shard = Math.floor(Math.random() * (args.shards ?? 1));
-    const prosemirror = await ctx.db
-      .query("prosemirrors")
-      .withIndex("name", (q) => q.eq("name", args.name).eq("shard", shard))
-      .unique();
-    if (prosemirror) {
-      await ctx.db.patch(prosemirror._id, {
-        value: prosemirror.value + args.count,
-      });
-    } else {
-      await ctx.db.insert("prosemirrors", {
-        name: args.name,
-        value: args.count,
-        shard,
-      });
+    const changes = await ctx.db
+      .query("deltas")
+      .withIndex("id_version", (q) =>
+        q.eq("id", args.id).gt("version", args.version)
+      )
+      .take(MAX_DELTA_FETCH);
+    if (changes.length > 0) {
+      const [steps, clientIds] = stepsAndClientIds(changes);
+      return { status: "needs-rebase", clientIds, steps } as const;
     }
+    await ctx.db.insert("deltas", {
+      id: args.id,
+      version: args.version + args.steps.length,
+      clientId: args.clientId,
+      steps: args.steps,
+    });
+    return { status: "synced" } as const;
   },
 });
 
-export const count = query({
-  args: { name: v.string() },
-  returns: v.number(),
+function stepsAndClientIds(deltas: Doc<"deltas">[]) {
+  const clientIds = [];
+  const steps = [];
+  for (const delta of deltas) {
+    for (const step of delta.steps) {
+      clientIds.push(delta.clientId);
+      steps.push(step);
+    }
+  }
+  return [steps, clientIds];
+}
+
+export const get = query({
+  args: {
+    id: v.string(),
+    version: v.optional(v.number()),
+    ignoreSteps: v.optional(v.boolean()),
+  },
+  returns: v.object({
+    snapshot: v.union(v.string(), v.null()),
+    steps: v.array(v.string()),
+    clientIds: v.array(v.string()),
+    version: v.number(),
+  }),
   handler: async (ctx, args) => {
-    const prosemirrors = await ctx.db
-      .query("prosemirrors")
-      .withIndex("name", (q) => q.eq("name", args.name))
-      .collect();
-    return prosemirrors.reduce(
-      (sum, prosemirror) => sum + prosemirror.value,
-      0
+    const snapshot = await ctx.db
+      .query("snapshots")
+      .withIndex("id_version", (q) =>
+        q.eq("id", args.id).lte("version", args.version ?? Infinity)
+      )
+      .order("desc")
+      .first();
+    if (!snapshot) {
+      return {
+        snapshot: null,
+        steps: [],
+        clientIds: [],
+        version: 0,
+      };
+    }
+    const [steps, clientIds] =
+      snapshot.version === args.version || args.ignoreSteps
+        ? [[], []]
+        : await fetchSteps(ctx, args.id, snapshot.version, args.version);
+    return {
+      snapshot: snapshot.content,
+      steps,
+      clientIds,
+      version: snapshot.version + steps.length,
+    };
+  },
+});
+
+async function fetchSteps(
+  ctx: QueryCtx,
+  id: string,
+  afterVersion: number,
+  targetVersion?: number
+) {
+  const deltas = await ctx.db
+    .query("deltas")
+    .withIndex("id_version", (q) =>
+      q
+        .eq("id", id)
+        .gt("version", afterVersion)
+        .lte("version", targetVersion ?? Infinity)
+    )
+    .take(MAX_DELTA_FETCH);
+  if (deltas.length > 0) {
+    const firstDelta = deltas[0];
+    if (firstDelta.version - firstDelta.steps.length > afterVersion) {
+      throw new Error(
+        `Missing steps ${afterVersion + 1}...${
+          firstDelta.version - firstDelta.steps.length
+        }`
+      );
+    } else if (firstDelta.version - firstDelta.steps.length < afterVersion) {
+      firstDelta.steps = firstDelta.steps.slice(
+        afterVersion - (firstDelta.version - firstDelta.steps.length)
+      );
+    }
+  }
+  const [steps, clientIds] = stepsAndClientIds(deltas);
+  if (deltas.length === MAX_DELTA_FETCH) {
+    console.warn(
+      `Max delta fetch reached: ${id} ${afterVersion}...${
+        targetVersion ?? "end"
+      } stopped at ${deltas[deltas.length - 1].version}`
     );
+    return [steps, clientIds];
+  }
+  const lastDelta = deltas[deltas.length - 1];
+  if (targetVersion && (!lastDelta || lastDelta.version < targetVersion)) {
+    const nextDelta = await ctx.db
+      .query("deltas")
+      .withIndex("id_version", (q) =>
+        q.eq("id", id).gt("version", lastDelta.version)
+      )
+      .first();
+    if (!nextDelta) {
+      throw new Error(
+        `Missing steps ${lastDelta ? lastDelta.version + 1 : afterVersion}...${targetVersion}`
+      );
+    }
+    for (const step of nextDelta.steps.slice(
+      0,
+      targetVersion - lastDelta.version
+    )) {
+      steps.push(step);
+      clientIds.push(nextDelta.clientId);
+    }
+  }
+  if (targetVersion && steps.length !== targetVersion - afterVersion) {
+    throw new Error(
+      `Steps mismatch ${afterVersion}...${targetVersion}: ${steps.length}`
+    );
+  }
+  return [steps, clientIds];
+}
+
+export const getSteps = query({
+  args: { id: v.string(), version: v.number() },
+  returns: v.object({
+    steps: v.array(v.string()),
+    clientIds: v.array(v.string()),
+    version: v.number(),
+  }),
+  handler: async (ctx, args) => {
+    const [steps, clientIds] = await fetchSteps(ctx, args.id, args.version);
+    return { steps, clientIds, version: args.version + steps.length };
   },
 });
--- src/component/schema.ts
+++ src/component/schema.ts
@@ -2,9 +2,16 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-  prosemirrors: defineTable({
-    name: v.string(),
-    value: v.number(),
-    shard: v.number(),
-  }).index("name", ["name", "shard"]),
+  snapshots: defineTable({
+    id: v.string(),
+    version: v.number(),
+    content: v.string(),
+  }).index("id_version", ["id", "version"]),
+  deltas: defineTable({
+    id: v.string(),
+    // The version of the last step.
+    version: v.number(),
+    clientId: v.string(),
+    steps: v.array(v.string()),
+  }).index("id_version", ["id", "version"]),
 });
