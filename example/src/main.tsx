4219fabec0417ecbd8e951fd033e98dcb16f210e
Ian Macartney <ianhmacartney@gmail.com>
2024-12-04 12:05:57 -0800

    assign a random doc ID on startup

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -7,8 +7,14 @@ const address = import.meta.env.VITE_CONVEX_URL;
 
 const convex = new ConvexReactClient(address);
 
+// Fetch the id from the URL hash, or make a new one
+const id = window.location.hash.slice(1) || crypto.randomUUID();
+if (window.location.hash !== `#${id}`) {
+  window.location.hash = `#${id}`;
+}
+
 createRoot(document.getElementById("root")!).render(
   <ConvexProvider client={convex}>
-    <App id="demo" />
+    <App id={id} />
   </ConvexProvider>
 );
