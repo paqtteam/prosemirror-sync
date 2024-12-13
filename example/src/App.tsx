7ec4e0117414c3575032425799e29a93c0478119
Ian Macartney <ianhmacartney@gmail.com>
2024-12-13 15:59:35 -0800

    clean up App.tsx

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEditor, EditorContent } from "@tiptap/react";
+import { EditorContent, EditorProvider } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import TaskList from "@tiptap/extension-task-list";
 import TaskItem from "@tiptap/extension-task-item";
@@ -7,10 +7,10 @@ import Placeholder from "@tiptap/extension-placeholder";
 import Typography from "@tiptap/extension-typography";
 import Underline from "@tiptap/extension-underline";
 import { api } from "../convex/_generated/api";
-import { Content, Extension } from "@tiptap/core";
+import { JSONContent } from "@tiptap/core";
 import { useSync } from "@convex-dev/prosemirror-sync/tiptap";
 
-const defaultExtensions = [
+const extensions = [
   StarterKit,
   TaskItem.configure({
     nested: true,
@@ -21,50 +21,66 @@ const defaultExtensions = [
   Underline,
 ];
 
+const EMPTY_DOC: JSONContent = {
+  type: "doc",
+  content: [
+    {
+      type: "paragraph",
+      content: [{ type: "text", text: "Write something..." }],
+    },
+  ],
+};
+
 function App(props: { id: string }) {
   const sync = useSync(api.example, props.id);
+  if (!sync.isLoading && sync.initialContent === null) {
+    sync.create(EMPTY_DOC);
+  }
   return (
     <>
-      <h1>Prosemirror + ConvexSync</h1>
+      <h1>ProseMirror + Convex Sync</h1>
+      <div>
+        This demonstrates syncing a ProseMirror document using Convex to provide
+        real-time collaborative editing.
+        <br />
+        It uses TipTap for the in-browser editing and support for inline
+        Markdown formatting, similar to Notion.
+        <br />
+        Share this URL to edit the same document, or test co-editing by opening
+        multiple tabs with the same URL.
+        <br />
+        The URL hash is the document ID.
+      </div>
       <div className="card">
-        {sync.isLoading ? (
-          <p>Loading...</p>
-        ) : sync.initialContent !== null ? (
-          <TipTap
-            syncExtension={sync.extension}
-            initialContent={sync.initialContent}
-          />
-        ) : (
-          <button
-            onClick={() => {
-              sync.create({
-                type: "doc",
-                content: [
-                  {
-                    type: "paragraph",
-                    content: [{ type: "text", text: "Write something..." }],
-                  },
-                ],
-              });
-            }}
+        {sync.initialContent !== null ? (
+          <EditorProvider
+            content={sync.initialContent}
+            extensions={[...extensions, sync.extension]}
           >
-            Create document (id: {props.id})
-          </button>
+            <EditorContent editor={null} />
+          </EditorProvider>
+        ) : (
+          <p>Loading...</p>
         )}
       </div>
-    </>
-  );
-}
-
-function TipTap(props: { syncExtension: Extension; initialContent: Content }) {
-  const editor = useEditor({
-    extensions: [...defaultExtensions, props.syncExtension],
-    content: props.initialContent,
-  });
-
-  return (
-    <>
-      <EditorContent editor={editor} />
+      <footer>
+        <p className="read-the-docs">
+          Powered by TipTap (ProseMirror) + Convex + Vite + React + TypeScript
+        </p>
+        <a
+          href="https://github.com/get-convex/prosemirror-sync"
+          className="github-button"
+        >
+          <svg fill="currentColor" viewBox="0 0 24 24">
+            <path
+              fillRule="evenodd"
+              d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+              clipRule="evenodd"
+            />
+          </svg>
+          Check it out on GitHub
+        </a>
+      </footer>
     </>
   );
 }
